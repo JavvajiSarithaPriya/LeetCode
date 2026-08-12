c5de91748677eac65c959ca5a0ba9bd1edb5fb5e
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-08-13 04:16:20 +0530

    Time: 47 ms (29.18%), Space: 30.5 MB (7.39%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        for i in freq:
+            if freq[i]>1:
+                return i
+                break
+        
+        
