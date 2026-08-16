f2b032a5afe8109f182081dfdfb9f326e9926b3f
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-08-16 14:43:09 +0530

    Time: 0 ms (100%), Space: 12.7 MB (31.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,27 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        low = 0
-        high = rows * cols - 1
-
-        while low <= high:
-
-            mid = (low + high) // 2
-
-            row = mid // cols
-            col = mid % cols
-
-            if matrix[row][col] == target:
+        rows=len(matrix)
+        cols=len(matrix[0])
+        low=0
+        high=rows*cols-1
+        while low<=high:
+            mid=(low+high)//2
+            row=mid//cols
+            col=mid%cols
+            if matrix[row][col]==target:
                 return True
-
-            elif matrix[row][col] < target:
-                low = mid + 1
-
+            elif matrix[row][col]<target:
+                low=mid+1
             else:
-                high = mid - 1
-
+                high=mid-1
         return False
