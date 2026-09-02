f5c9f130e7c6686d7a9f6771e848bdfd1515b536
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-09-02 22:09:11 +0530

    Time: 0 ms (100%), Space: 12.6 MB (61.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,23 +1,21 @@
 class Solution(object):
-    def nextGreaterElement(self,nums1, nums2):
-        stack = []
-        nge_map = {}
-
-    # Step 1: Process nums2
-        for i in range(len(nums2) - 1, -1, -1):
-            while stack and stack[-1] <= nums2[i]:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack=[]
+        nge={}
+        for i in  range(len(nums2)-1,-1,-1):
+            while stack and stack[-1]<=nums2[i]:
                 stack.pop()
-
             if stack:
-                nge_map[nums2[i]] = stack[-1]
+                nge[nums2[i]]=stack[-1]
             else:
-                nge_map[nums2[i]] = -1
-
+                nge[nums2[i]]=-1
             stack.append(nums2[i])
-
-    # Step 2: Build answer for nums1
-        result = []
+        ans=[]
         for num in nums1:
-            result.append(nge_map[num])
-
-        return result
+            ans.append(nge[num])
+        return ans
