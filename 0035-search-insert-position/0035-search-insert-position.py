1f76894a90bbf3b89d6ae4ddf7cbe49f8a5ae7e4
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-09-20 21:45:54 +0530

    Time: 0 ms (100%), Space: 13 MB (31.28%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -5,15 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        low=0
-        high=len(nums)-1
-        while low<=high:
-            mid=(low+high)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]<target:
-                low=mid+1
-            else:
-                high=mid-1
-        return low
+        for i in range(len(nums)):
+            if nums[i]>=target:
+                return i
+        return len(nums)
         
