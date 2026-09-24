dc11ece797e6a0aa6f54f1a79620dafdbea01824
Javvaji Saritha Priya <23BQ1A1244@vvit.net>
2026-09-24 14:59:35 +0530

    Time: 11 ms (18.05%), Space: 12.3 MB (93.61%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isBalanced(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        e=0
+        o=0
+        for i in range(len(num)):
+            if i%2==0:
+                e+=int(num[i])
+            else:
+                o+=int(num[i])
+        return e==o
+        
